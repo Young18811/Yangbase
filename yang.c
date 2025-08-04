23124b8e76cd47c66cdbf55bcacc0a5a533c9766
Young <2385212397@qq.com>
2025-08-04 17:01:50 +0800

    Update yang.c

--- yang.c
+++ yang.c
@@ -2,8 +2,11 @@
 int main()
 {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     printf("hello world! 1\n");
+=======
     printf("hello world! y");
+>>>>>>> Stashed changes
 =======
     printf("hello world! y");
 >>>>>>> Stashed changes
